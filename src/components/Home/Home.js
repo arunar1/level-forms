9a53a5c9c00a0d84cfec2ed3a73b6ee5f14241af
Arun <arunarofficial00@gmail.com>
2024-06-22 19:10:22 +0530

    feat : home page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <div>Home</div>
+   <div className="home-container">
+    <div className="center-container">
+        <h1>Let's</h1>
+        <Button>Start</Button>
+    </div>
+   </div>
   )
 }
