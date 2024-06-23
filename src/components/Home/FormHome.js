5feea8b5f24967a971dbb6d67f44123d1234e4f9
Arun <arunarofficial00@gmail.com>
2024-06-23 14:42:07 +0530

    faet : added the home page to select forms

--- src/components/Home/FormHome.js
+++ src/components/Home/FormHome.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export default function FormHome() {
   return (
     <div className="form-container">
-        
+        <Button>Level-1</Button>
+        <Button>Level-2</Button>
+        <Button>Level-3</Button>
+
     </div>
   )
 }
