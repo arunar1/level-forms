9cfe80c67f558f21fd520eb016f6977f01fb1772
Arun <arunarofficial00@gmail.com>
2024-06-24 22:05:13 +0530

    commit

--- src/components/Home/FormHome.js
+++ src/components/Home/FormHome.js
@@ -4,9 +4,9 @@ import { Button } from 'react-bootstrap'
 export default function FormHome() {
   return (
     <div className="form-container">
-        <Button>Level-1</Button>
-        <Button>Level-2</Button>
-        <Button>Level-3</Button>
+        <Button>Level 1</Button>
+        <Button>Level 2</Button>
+        <Button>Level 3</Button>
 
     </div>
   )
