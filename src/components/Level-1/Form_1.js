749b33a35b498f2dd730132b829d11a0d92c725b
Arun <arunarofficial00@gmail.com>
2024-06-25 22:05:49 +0530

    added : basic functional code

---
+++ src/components/Level-1/Form_1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Form_1() {
+  return (
+    <div>Form_1</div>
+  )
+}
---
+++ src/components/Level-2/Form_2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Form_2() {
+  return (
+    <div>Form_2</div>
+  )
+}
